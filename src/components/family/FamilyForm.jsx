e8ad81294d40999e89950d17b64f3b2bf31fb505
Yurii Kozyk <y.kozyk@gmail.com>
2026-07-13 11:48:06 +0300

    dissable autofill from prev person

--- src/components/family/FamilyForm.jsx
+++ src/components/family/FamilyForm.jsx
@@ -269,16 +269,21 @@ const RegisterForm = ({ submitAction, isHouseholder, personValues = null, closeA
   /** provide initial data from householder to avoid user adding the same data 
    *  multiple times
   */
-  if (!isHouseholder && !personValues && family[0]) {
-    initialValues = {
-      ...initialValues,
-      vpo_city: family[0].vpo_city,
-      vpo_street: family[0].vpo_street,
-      vpo_bud: family[0].vpo_bud,
-      vpo_corp: family[0].vpo_corp,
-      vpo_apartment: family[0].vpo_apartment,
-    }
-  }
+  // if (family && family[0] && family[0].address_city && !isHouseholder && !personValues) {
+  //   initialValues = {
+  //     ...initialValues,
+  //     address_city: family[0].address_city,
+  //     address_street: family[0].address_street,
+  //     address_number: family[0].address_number,
+  //     address_corpus: family[0].address_corpus,
+  //     address_room: family[0].address_room,
+  //     vpo_city: family[0].vpo_city,
+  //     vpo_street: family[0].vpo_street,
+  //     vpo_bud: family[0].vpo_bud,
+  //     vpo_corp: family[0].vpo_corp,
+  //     vpo_apartment: family[0].vpo_apartment,
+  //   }
+  // }
 
   // const validateDocsFields = async (values, setFieldError) => {
   //   const fieldsToValidate = ['document', 'tax_number', 'vpo_number'];
